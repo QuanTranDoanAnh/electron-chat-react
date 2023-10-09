c1e1f01403661e6ce1e365c46143b1c3dc44c51a
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-10-09 13:56:48 +0900

    Electron features

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// Main process
 const { app, BrowserWindow } = require('electron')
 
 function createMainWindow() {
+  // Browser Window <-- Renderer process
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -10,6 +12,19 @@ function createMainWindow() {
     }
   })
   mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(createMainWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('active', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
