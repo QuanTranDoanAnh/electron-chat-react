1f871beb04db87a8bd47c08a0ecf7f04fbb3eaaa
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-10-09 20:25:51 +0900

    Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(<h1>I am the React App</h1>, document.getElementById('electronChat'))
